f9f88be8af7adade0f5c1bc273537c024103812e
crate <crateimage@gmail.com>
2025-05-23 11:41:26 -0500

    wiki except

--- garfpy/wiki.py
+++ garfpy/wiki.py
@@ -5,5 +5,6 @@ async def wikisum(search_term):
         summary = wikipedia.summary(search_term)
         return summary
     
-    except wikipedia.exceptions.DisambiguationError as e:
+    # except wikipedia.exceptions.DisambiguationError as e:
+    except Exception as e:
         return e
